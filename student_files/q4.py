1f0925843af38d15807bb266f9b00a3b48778b70
sufi4414 <115709940+sufi4414@users.noreply.github.com>
2024-04-12 23:13:11 +0800

    Update q4.py

--- student_files/q4.py
+++ student_files/q4.py
@@ -1,6 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
-
+from pyspark import SparkContext, SparkConf
 # you may add more import if you need to
 
 
@@ -9,3 +9,40 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 4").getOrCreate()
 # YOUR CODE GOES BELOW
+def count_restaurant(input_path):
+  try:
+    conf = SparkConf().setAppName("Part 1 Question 4")
+    sc = SparkContext(conf=conf)
+    spark = SparkSession(sc)
+
+    df = spark.read.option("header", True).csv(input_path)
+
+
+    df = df.withColumn("Cuisine Style", F.regexp_replace("Cuisine Style", "^\\[|\\]$", ""))
+    df = df.withColumn("Cuisine Style", F.split(F.col("Cuisine Style"), ",\s*"))
+
+
+    df_exploded = df.withColumn("Cuisine", F.explode("Cuisine Style"))
+    df_exploded = df_exploded.withColumn("Cuisine", F.regexp_replace("Cuisine", "'", ""))
+    df_exploded = df_exploded.withColumn("Cuisine", F.trim("Cuisine"))
+
+
+    result_df = (
+        df_exploded.groupBy("City", "Cuisine")
+        .count()
+        .orderBy("City", F.col("count").desc())
+    )
+
+
+    result_df = result_df.select(
+        F.col("City").alias("City"),
+        F.col("Cuisine").alias("Cuisine"),
+        F.col("count")
+    )
+
+    result_df.show()
+
+  finally:
+    sc.stop()
+
+count_restaurant(input_path)
